b830821c3c118e2a8cb6bd997f33baa11d673c38
Jwnrobinson <89938078+Jwnrobinson@users.noreply.github.com>
2021-09-19 20:25:05 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-Let friends = [james, milo, jack, steve, sarah];
+window.addEventListener('DOMContentLoaded', function (){
+    let button = document.getElementById("button");
 
-for (let f = 0; f < friends.length; f++) {
-    const element = array[i]; {
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            
-        }
-    }
-            
-}
 
-console.log(friends);
+    button.addEventListener ("click", function(){
+        let friends = ["james", "milo", "jack", "steve", "sarah"];
+
+        for (let f = 0; f < friends.length; f++) {
+            
+                const div = document.createElement('div');
+                const h3 = document.createElement('h3')
+
+                document.body.appendChild(div);
+                div.className = "Friend";
+                div.innerHTML="<h3 class='name'>" + friends[f].toUpperCase() + "</h3>";
+                for (let j=99; j>0; j--){
+                    if(j===1){
+                   const p = document.createElement('p');
+                   p.innerHTML = j + " line of code in the file," + j + " line of code;" + friends[f] + " strikes one out, clears it all out, no more lines of codein the file " ;
+                   div.appendChild(p);
+                        
+                    } else{
+                        const p = document.createElement("p");
+                        p.innerHTML = j + " lines of code in the file, " + j + " lines of code; " + friends[f] + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file " ;
+                        div.appendChild(p);
+                    }
+                }
+
+                    
+                }
+            }
+                    
+        ,
+    )
+
+
+
+
+
+})
